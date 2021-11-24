32ef801e80034e33cf1b418f9d18b6bc0b02a4ca
João Vitor Gomes <joaovitorgomes95@gmail.com>
2021-11-24 20:51:20 -0300

    ok

--- src/components/shared/Footer/Footer.js
+++ src/components/shared/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="w-100 bg-primary text-light d-flex justify-content-center align-items-center">
+    <footer className="w-100 bg-dark text-light d-flex justify-content-center align-items-center">
       <div className="container">
         <p className="text-center"> Desenvolvido por João Vitor Gomes</p>
       </div>
